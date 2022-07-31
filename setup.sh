a9d039c09ced6845e66c9ebc80fc0145c257396f
François Guérin <francois.guerin@gravity.earth>
2022-07-31 14:33:26 +0200

    Color command

--- setup.sh
+++ setup.sh
@@ -4,47 +4,48 @@
 # - cURL
 # - apt
 
-set -x
-
 CODE_DIR=$HOME/Projects/Code
 GIT_BRANCH=master
 GIT_PROJECT=setup
 GIT_USER=HuffmanTree
+NODEJS_VERSION=16
 NVM_DIR=$HOME/.nvm
 PACKAGES=$(curl -s https://raw.githubusercontent.com/$GIT_USER/$GIT_PROJECT/$GIT_BRANCH/packages.txt)
-PS4="[$GIT_PROJECT] >>> "
 REPOSITORIES=$(curl -s https://raw.githubusercontent.com/$GIT_USER/$GIT_PROJECT/$GIT_BRANCH/repositories.txt)
 
+run() {
+    printf "[$GIT_PROJECT] $ \033[01;32m$1\n\033[00m"
+    $1
+}
+
 # 1. Install apt packages
-sudo apt update
-sudo apt install $PACKAGES
+run "sudo apt update"
+run "sudo apt install $PACKAGES"
 
 # 2. Install nvm + node + npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source $HOME/.bashrc
-nvm install 16
-nvm use 16
-nvm alias default 16
+run "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash"
+run "source $HOME/.bashrc"
+run "nvm install $NODEJS_VERSION"
+run "nvm use $NODEJS_VERSION"
+run "nvm alias default $NODEJS_VERSION"
 
 # 3. Install projects
 
 # 3.1 Make ~/Projects/Code directory
-mkdir -p $CODE_DIR
+run "mkdir -p $CODE_DIR"
 
 # 3.2 Clone repositories and set them up
 for REPOSITORY in ${REPOSITORIES[@]}
 do
     if [ ! -d $CODE_DIR/$REPOSITORY ]
     then
-	git clone git@github.com:$GIT_USER/$REPOSITORY.git $CODE_DIR/$REPOSITORY
+	run "git clone git@github.com:$GIT_USER/$REPOSITORY.git $CODE_DIR/$REPOSITORY"
 
 	if [ -f $CODE_DIR/$REPOSITORY/setup.sh ];
 	then
-	    chmod +x $CODE_DIR/$REPOSITORY/setup.sh
-	    $CODE_DIR/$REPOSITORY/setup.sh
+	    run "chmod +x $CODE_DIR/$REPOSITORY/setup.sh"
+	    run "$CODE_DIR/$REPOSITORY/setup.sh"
 	fi
 
     fi
 done
-
-set +x
